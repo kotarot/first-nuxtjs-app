d76c3049fdcddaac7d3cc4b67846bfdf898415e2
Kotaro Terada <kotarot@apache.org>
2021-08-08 00:37:05 +0900

    Add 'add', 'remove', and 'changeState'

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,112 @@
 <template>
-  <Tutorial/>
+  <section class="container mx-auto">
+    <div class="flex justify-center py-5">
+      <h1 class="text-5xl">TODO List App</h1>
+    </div>
+    <div class="flex justify-center py-5">
+      <div class="add-area">
+        <input type="text" name="add-task" id="add-task" v-model="content" placeholder="Input a task">
+        <button id="add-button" class="btn btn-add" @click="add">Add</button>
+      </div>
+    </div>
+    <div class="flex justify-center py-5">
+      <div class="filter">
+        <button class="btn btn-filter btn-filter-is-active">ALL</button>
+        <button class="btn btn-filter">TODO</button>
+        <button class="btn btn-filter">DOING</button>
+        <button class="btn btn-filter">DONE</button>
+      </div>
+    </div>
+    <div class="flex justify-center py-5">
+      <table class="table-auto lists">
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Date</th>
+            <th>Status</th>
+            <th>Operation</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in todos" :key="index">
+            <td>{{ item.content }}</td>
+            <td>{{ item.created }}</td>
+            <td>
+              <button class="btn"
+                  v-bind:class="{
+                  'btn-todo': item.state == 'TODO',
+                  'btn-doing': item.state == 'DOING',
+                  'btn-done': item.state == 'DONE'}"
+                  @click="changeState(item)">
+                {{ item.state }}
+              </button>
+            </td>
+            <td><button class="btn btn-remove" @click="remove(item)">Remove</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {}
+import {mapState} from 'vuex'
+
+export default {
+  data: () => {
+    return {content: ''}
+  },
+  computed: {
+    ...mapState(['todos'])
+  },
+  methods: {
+    add: function() {
+      if (this.content != '') {
+        this.$store.commit('add', {content: this.content})
+        this.content = ''
+      }
+    },
+    remove: function(item) {
+      this.$store.commit('remove', item)
+    },
+    changeState: function(item) {
+      this.$store.commit('changeState', item)
+    },
+  },
+}
 </script>
+
+<style>
+  #add-task {
+    @apply shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight flex-grow;
+  }
+
+  .btn {
+    @apply py-2 px-4 font-semibold rounded-lg shadow-md;
+  }
+  .btn-add {
+    @apply text-white bg-blue-500 hover:bg-blue-600;
+  }
+  .btn-filter {
+    @apply text-gray-500 border-gray-500 hover:bg-gray-200;
+  }
+  .btn-filter-is-active {
+    @apply text-white bg-gray-500 hover:bg-gray-600;
+  }
+  .btn-todo {
+    @apply text-green-500 border-green-500 hover:bg-green-100;
+  }
+  .btn-doing {
+    @apply text-green-500 border-green-500 bg-green-100 hover:bg-green-200;
+  }
+  .btn-done {
+    @apply text-white bg-green-500 hover:bg-green-600;
+  }
+  .btn-remove {
+    @apply text-red-500 border-red-500 hover:bg-red-100;
+  }
+
+  table th, td {
+    @apply py-2 px-4;
+  }
+</style>
